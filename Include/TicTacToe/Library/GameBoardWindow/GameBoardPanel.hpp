086d484ebd9ae26db30882a558b684e27daac540
Kamal <kamal@eidolonsystems.com>
2020-12-02 18:06:49 -0500

    Added starter code for the game board UI.

---
+++ Include/TicTacToe/Library/GameBoardWindow/GameBoardPanel.hpp
@@ -0,0 +1,36 @@
+#ifndef TIC_TAC_TOE_GAME_BOARD_PANEL_HPP
+#define TIC_TAC_TOE_GAME_BOARD_PANEL_HPP
+#include <QWidget>
+#include "TicTacToe/Library/Board.hpp"
+#include "TicTacToe/Library/TicTacToe.hpp"
+
+namespace TicTacToe {
+
+  /** Displays a game board. */
+  class GameBoardPanel : public QWidget {
+    public:
+
+      /**
+       * Signals a tile has been clicked.
+       * @param x The x-coordinate of the tile clicked.
+       * @param x The y-coordinate of the tile clicked.
+       */
+      using ClickedSignal = Signal<void (int x, int y)>;
+
+      /**
+       * Constructs a GameBoardPanel displaying a given Board.
+       * @param board The Board to display.
+       * @param parent The parent widget.
+       */
+      explicit GameBoardPanel(const Board& board, QWidget* parent = nullptr);
+
+      /** Indicates the displayed Board has been updated. */
+      void refresh_board();
+
+      /** Connects a slot to the ClickedSignal. */
+      boost::signals2::connection connect_clicked_signal(
+        const ClickedSignal::slot_type& slot) const;
+  };
+}
+
+#endif
---
+++ Include/TicTacToe/Library/GameBoardWindow/TokenTile.hpp
@@ -0,0 +1,44 @@
+#ifndef TIC_TAC_TOE_TOKEN_TILE_HPP
+#define TIC_TAC_TOE_TOKEN_TILE_HPP
+#include <QWidget>
+#include "TicTacToe/Library/Board.hpp"
+#include "TicTacToe/Library/TicTacToe.hpp"
+
+namespace TicTacToe {
+
+  /** Displays a single board tile. */
+  class TokenTile : public QWidget {
+    public:
+
+      /** Signals the tile has been clicked. */
+      using ClickedSignal = Signal<void ()>;
+
+      /**
+       * Constructs a TokenTile display no token.
+       * @param parent The parent widget.
+       */
+      explicit TokenTile(QWidget* parent = nullptr);
+
+      /**
+       * Constructs a TokenTile displaying a token.
+       * @param token The token to display.
+       * @param parent The parent widget.
+       */
+      explicit TokenTile(Board::Token token, QWidget* parent = nullptr);
+
+      /** Returns the displayed token. */
+      Board::Token get_token() const;
+
+      /** Sets the token to display. */
+      void set_token(Board::Token token);
+
+      /** Connects a slot to the ClickedSignal. */
+      boost::signals2::connection connect_clicked_signal(
+        const ClickedSignal::slot_type& slot) const;
+
+    private:
+      mutable ClickedSignal m_clickedSignal;
+  };
+}
+
+#endif
