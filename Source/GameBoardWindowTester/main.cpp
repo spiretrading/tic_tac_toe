3a481e2735a9af691f351d6f55bd81c0cde09460
Shaobin <shaobin@spiretrading.com>
2020-12-09 14:45:24 -0500

    style: snake_case for variable names and function names

--- Source/GameBoardWindowTester/main.cpp
+++ Source/GameBoardWindowTester/main.cpp
@@ -9,52 +9,52 @@
 using namespace TicTacToe;
 
 namespace {
-  QWidget* MakeGameBoardWindow(const Board& board) {
+  QWidget* make_game_board_window(const Board& board) {
     auto window = new QWidget();
     auto board_window = new GameBoardWindow(board, Board::Token::X, window);
     window->setFixedSize(780, 514);
     return window;
   }
 
-  QWidget* MakeControllerWindow(QWidget* gameBoardWindow) {
-    auto board_window = gameBoardWindow->findChild<GameBoardWindow*>();
+  QWidget* make_controller_window(QWidget* game_board_window) {
+    auto board_window = game_board_window->findChild<GameBoardWindow*>();
     auto window = new QWidget();
     window->setWindowTitle(QObject::tr("Window Controller"));
     auto layout = new QGridLayout(window);
     window->setLayout(layout);
     auto player_label = new QLabel(QObject::tr("Player: "), window);
     layout->addWidget(player_label, 0, 0);
-    auto turn_combobox = new QComboBox(window);
-    turn_combobox->addItem(QObject::tr("X"));
-    turn_combobox->addItem(QObject::tr("O"));
+    auto turn_combo_box = new QComboBox(window);
+    turn_combo_box->addItem(QObject::tr("X"));
+    turn_combo_box->addItem(QObject::tr("O"));
     board_window->set_turn(Board::Token::X);
-    layout->addWidget(turn_combobox, 0, 1);
+    layout->addWidget(turn_combo_box, 0, 1);
     auto unlocked_label = new QLabel(QObject::tr("Unlocked Badges: "), window);
     layout->addWidget(unlocked_label, 1, 0);
-    auto unlocked_spinBox = new QSpinBox(window);
-    unlocked_spinBox->setMinimum(0);
-    unlocked_spinBox->setMaximum(9);
-    QObject::connect(turn_combobox, &QComboBox::currentTextChanged,
+    auto unlocked_spin_box = new QSpinBox(window);
+    unlocked_spin_box->setMinimum(0);
+    unlocked_spin_box->setMaximum(9);
+    QObject::connect(turn_combo_box, &QComboBox::currentTextChanged,
       [=] (const QString& text) {
         if(text == "X") {
           board_window->set_turn(Board::Token::X);
           board_window->get_x_badge_panel().set_score(
-            unlocked_spinBox->value());
+            unlocked_spin_box->value());
         } else {
           board_window->set_turn(Board::Token::O);
           board_window->get_o_badge_panel().set_score(
-            unlocked_spinBox->value());
+            unlocked_spin_box->value());
         }
       });
-    QObject::connect(unlocked_spinBox, qOverload<int>(&QSpinBox::valueChanged),
+    QObject::connect(unlocked_spin_box, qOverload<int>(&QSpinBox::valueChanged),
       [=] (int value) {
-        if(turn_combobox->currentText() == "X") {
+        if(turn_combo_box->currentText() == "X") {
           board_window->get_x_badge_panel().set_score(value);
         } else {
           board_window->get_o_badge_panel().set_score(value);
         }
       });
-    layout->addWidget(unlocked_spinBox, 1, 1);
+    layout->addWidget(unlocked_spin_box, 1, 1);
     return window;
   }
 }  // namespace
@@ -65,17 +65,17 @@ int main(int argc, char** argv) {
   application->setApplicationName(QObject::tr("Game Board Window Tester"));
   initialize_resources();
   auto board = Board();
-  auto gameBoardWindow = MakeGameBoardWindow(board);
-  auto board_window = gameBoardWindow->findChild<GameBoardWindow*>();
+  auto game_board_window = make_game_board_window(board);
+  auto board_window = game_board_window->findChild<GameBoardWindow*>();
   board_window->get_panel().connect_clicked_signal([&] (int x, int y) {
     auto token = board.get_piece(x, y);
     auto next_token = (static_cast<int>(token) + 1) % 3;
     board.set_piece(x, y, static_cast<Board::Token>(next_token));
     board_window->get_panel().refresh_board();
   });
-  gameBoardWindow->activateWindow();
-  gameBoardWindow->show();
-  auto controller = MakeControllerWindow(gameBoardWindow);
+  game_board_window->activateWindow();
+  game_board_window->show();
+  auto controller = make_controller_window(game_board_window);
   controller->show();
   application->exec();
 }
