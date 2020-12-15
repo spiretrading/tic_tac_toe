e7be13215035065dfb411dc05a1bf7ad8d9fdd0c
Shaobin <shaobin@spiretrading.com>
2020-12-15 17:36:01 -0500

    connect clicked() signal of button

--- Source/Library/DrawConditionWindow.cpp
+++ Source/Library/DrawConditionWindow.cpp
@@ -72,6 +72,10 @@ DrawConditionWindow::DrawConditionWindow(QWidget* parent)
   layout->addWidget(back_button, 0, Qt::AlignCenter);
   layout->addSpacing(125);
   setLayout(layout);
+  QObject::connect(start_button, &QPushButton::clicked,
+    this, [this] { m_start_signal(); });
+  QObject::connect(back_button, &QPushButton::clicked,
+    this, [this] { m_back_signal(); });
 }
 
 connection DrawConditionWindow::connect_start_signal(
